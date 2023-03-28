9b87127d2b6a372038de12ce74ce7b39d4b540d1
MyFRA <tomyntapss@gmail.com>
2023-03-28 10:40:29 +0700

    Wa Notification

---
+++ resources/js/Pages/Frontend/ProductValidation/QrProduct.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="visible-print text-center">
+        {{ qr }}
+        <p>Scan me to return to the original page.</p>
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        qr
+    }
+}
+</script>
